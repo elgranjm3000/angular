d1ff44a32fc21127265b623832ff74fbbaeabf0e
joseph muentes <elgranjm3000@gmail.com>
2022-11-22 16:41:42 -0400

    listado de articulo en blog

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ArticleService } from '../../services/article.service';
-
+import { Article } from '../../models/article';
+import { Global  } from '../../services/global';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
@@ -9,6 +10,9 @@ import { ArticleService } from '../../services/article.service';
 })
 export class BlogComponent implements OnInit {
 
+  public articles?:Article[];
+  public url?: string;
+
   constructor(
     private _articleService:ArticleService
 
@@ -16,9 +20,15 @@ export class BlogComponent implements OnInit {
 
   ngOnInit(): void {
     //console.log(this._articleService.pruebas());
+    this.url = Global.url;
     this._articleService.getArticles().subscribe(
       response =>{
         console.log(response);
+        if(response.article){
+          this.articles = response.article;
+        }else{
+
+        }
       },
       error => {
         console.log(error);
